294418196a8be9a9c814972b7566b7240486ec41
Blastz13 <59578914+Blastz13@users.noreply.github.com>
2022-04-06 14:39:02 +0300

    Init commit

---
+++ app/src/main/java/com/example/shop/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
---
+++ app/src/main/java/com/example/shop/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        },  3500);
+    }
+}
